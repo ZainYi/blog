fd384b9d6a77901b7b9c0ea3a7b9a94b1c308667
zuoyi615 <zuoyi615@gmail.com>
2018-06-14 17:07:06 +0800

    子分类

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -19,12 +19,32 @@ module.exports = {
         link: '/'
       },
       {
-        text: 'JavaScript',
-        link: '/JavaScript/'
+        text: 'ECMAScript',
+        items: [{
+            text: 'ECMAScript5.1',
+            link: '/ECMAScript/ECMAScript5/'
+          },
+          {
+            text: 'ECMAScript2015+',
+            link: '/ECMAScript/ECMAScript2015/'
+          }
+        ]
       },
       {
         text: 'CSS3',
-        link: '/CSS3/'
+        items: [{
+            text: 'CSS3 布局',
+            link: '/CSS3/Layout/'
+          },
+          {
+            text: 'CSS3 新特性',
+            link: '/CSS3/Feature/'
+          },
+          {
+            text: 'CSS3 基础',
+            link: '/CSS3/Basic/'
+          }
+        ]
       },
       {
         text: 'HTML5',
@@ -43,13 +63,24 @@ module.exports = {
         link: '/HTTP/'
       },
       {
+        text: 'Tool',
+        link: '/Tool/'
+      },
+      {
         text: '博主',
         link: '/About/'
       }
     ],
     sidebar: [
       '/',
-      '/JavaScript/',
+      {
+        title: 'ECMAScript',
+        children: [
+          '/ECMAScript/',
+          '/ECMAScript/ECMAScript5/',
+          '/ECMAScript/ECMAScript2015/'
+        ]
+      },
       {
         title: 'CSS3',
         children: ['/CSS3/Layout/', '/CSS3/Feature/', '/CSS3/Basic/']
--- CSS3/Layout/README.md
+++ CSS3/Layout/README.md
@@ -1,5 +1,4 @@
-# 布局
+# CSS3 布局
 
-## 目录
-
-1. [经典布局](/CSS3/Layout/01-经典布局.md)
+1. [经典布局](/CSS3/Layout/01-经典布局.md) —— 2016/06/14
+1. [flexbox 布局](/CSS3/Layout/flexbox布局.md) —— 2016/08/14
